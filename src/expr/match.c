1d1b818f3d6713b0a79bb1c0bda71446f08428af
Arturo Borrero <arturo.borrero.glez@gmail.com>
2013-06-03 22:44:55 +0200

    expr: xml: don't print target and match info
    This is binary layout of the iptables target/match, we can do nothing
    with it at this moment. Let's get rid of it.
    
    Signed-off-by: Arturo Borrero Gonzalez <arturo.borrero.glez@gmail.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- src/expr/match.c
+++ src/expr/match.c
@@ -245,20 +245,10 @@ static int nft_rule_expr_match_snprintf_xml(char *buf, size_t len,
 					    struct nft_expr_match *mt)
 {
 	int ret, size=len;
-	int i;
 	int offset = 0;
-	uint8_t *data = (uint8_t *)mt->data;
 
-	ret = snprintf(buf, len, "<name>%s</name><rev>%u</rev><info>0x",
+	ret = snprintf(buf, len, "<name>%s</name><rev>%u</rev>",
 				mt->name, mt->rev);
-	SNPRINTF_BUFFER_SIZE(ret, size, len, offset);
-
-	for (i=0; i < mt->data_len; i++) {
-	        ret = snprintf(buf+offset, len, "%x", data[i] & 0xff);
-		SNPRINTF_BUFFER_SIZE(ret, size, len, offset);
-	}
-
-	ret = snprintf(buf+offset, len, "</info>");
 	SNPRINTF_BUFFER_SIZE(ret, size, len, offset);
 
 	return offset;
--- src/expr/target.c
+++ src/expr/target.c
@@ -249,20 +249,10 @@ int nft_rule_exp_target_snprintf_xml(char *buf, size_t len,
 				struct nft_expr_target *tg)
 {
 	int ret, size=len;
-	int i;
 	int offset = 0;
-	uint8_t *data = (uint8_t *)tg->data;
 
-	ret = snprintf(buf, len, "<name>%s</name><rev>%u</rev><info>0x",
+	ret = snprintf(buf, len, "<name>%s</name><rev>%u</rev>",
 			tg->name, tg->rev);
-	SNPRINTF_BUFFER_SIZE(ret, size, len, offset);
-
-	for (i=0; i < tg->data_len; i++) {
-		ret = snprintf(buf+offset, len, "%x", data[i] & 0xff);
-		SNPRINTF_BUFFER_SIZE(ret, size, len, offset);
-	}
-
-	ret = snprintf(buf+offset, len, "</info>");
 	SNPRINTF_BUFFER_SIZE(ret, size, len, offset);
 
 	return offset;
