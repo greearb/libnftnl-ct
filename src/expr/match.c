0fdb474c270d4574a2da3eef6042151bd8512698
Ana Rey <anarey@gmail.com>
2014-06-24 19:15:02 +0200

    expr: match: Use nft_rule_expr_set_* in the xml parsing code
    Code refactoring to use nft_rule_expr_set_* in parse functions.
    
    Signed-off-by: Ana Rey <anarey@gmail.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- src/expr/match.c
+++ src/expr/match.c
@@ -194,17 +194,13 @@ static int nft_rule_expr_match_xml_parse(struct nft_rule_expr *e, mxml_node_t *t
 					 struct nft_parse_err *err)
 {
 #ifdef XML_PARSING
-	struct nft_expr_match *mt = nft_expr_data(e);
 	const char *name;
 
 	name = nft_mxml_str_parse(tree, "name", MXML_DESCEND_FIRST,
 				  NFT_XML_MAND, err);
 	if (name == NULL)
 		return -1;
-
-	strncpy(mt->name, name, XT_EXTENSION_MAXNAMELEN);
-	mt->name[XT_EXTENSION_MAXNAMELEN-1] = '\0';
-	e->flags |= (1 << NFT_EXPR_MT_NAME);
+	nft_rule_expr_set_str(e, NFT_EXPR_MT_NAME, name);
 
 	/* mt->info is ignored until other solution is reached */
 
