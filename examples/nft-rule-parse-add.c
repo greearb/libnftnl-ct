0260882c83780243078538a5bb14033258326f79
Arturo Borrero <arturo.borrero.glez@gmail.com>
2014-09-23 13:15:21 +0200

    examples: merge nft-rule-{xml|json}-add.c
    Merge the two examples in just one. An input argument choose the format to use.
    
    Signed-off-by: Arturo Borrero Gonzalez <arturo.borrero.glez@gmail.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- examples/nft-rule-parse-add.c
+++ examples/nft-rule-parse-add.c
@@ -1,8 +1,4 @@
 /*
- * (C) 2013 by Álvaro Neira Ayuso <alvaroneay@gmail.com>
- *
- * Based on nft-rule-xml-add from:
- *
  * (C) 2013 by Pablo Neira Ayuso <pablo@netfilter.org>
  * (C) 2013 by Arturo Borrero Gonzalez <arturo.borrero.glez@gmail.com>
  *
@@ -10,14 +6,18 @@
  * under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
+ *
+ * This code has been sponsored by Sophos Astaro <http://www.sophos.com>
  */
 
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+#include <stddef.h>	/* for offsetof */
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <string.h>
-#include <netinet/in.h>
 #include <fcntl.h>
 #include <errno.h>
 
@@ -27,34 +27,23 @@
 #include <libmnl/libmnl.h>
 #include <libnftnl/rule.h>
 
-int main(int argc, char *argv[])
+static struct nft_rule *rule_parse_file(const char *file, uint16_t format)
 {
-	struct mnl_socket *nl;
-	char buf[MNL_SOCKET_BUFFER_SIZE];
-	struct nlmsghdr *nlh;
-	uint32_t portid, seq;
-	struct nft_rule *r = NULL;
-	int ret, fd;
-	uint8_t family;
-	char json[4096];
-	char reprint[4096];
+	int fd;
+	struct nft_rule *r;
 	struct nft_parse_err *err;
+	char data[4096];
 
-	if (argc < 2) {
-		printf("Usage: %s <json-file>\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-
-	fd = open(argv[1], O_RDONLY);
+	fd = open(file, O_RDONLY);
 	if (fd < 0) {
 		perror("open");
-		exit(EXIT_FAILURE);
+		return NULL;
 	}
 
-	if (read(fd, json, sizeof(json)) < 0) {
+	if (read(fd, data, sizeof(data)) < 0) {
 		perror("read");
 		close(fd);
-		exit(EXIT_FAILURE);
+		return NULL;
 	}
 	close(fd);
 
@@ -70,16 +59,53 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	if (nft_rule_parse(r, NFT_PARSE_JSON, json, err) < 0) {
-		nft_parse_perror("Unable to parse JSON file", err);
+	if (nft_rule_parse(r, format, data, err) < 0) {
+		nft_parse_perror("Unable to parse file", err);
+		nft_parse_err_free(err);
+		nft_rule_free(r);
+		return NULL;
+	}
+
+	nft_rule_attr_unset(r, NFT_RULE_ATTR_HANDLE);
+
+	nft_parse_err_free(err);
+	return r;
+}
+
+int main(int argc, char *argv[])
+{
+	struct mnl_socket *nl;
+	char buf[MNL_SOCKET_BUFFER_SIZE];
+	struct nlmsghdr *nlh;
+	uint32_t portid, seq;
+	struct nft_rule *r;
+	int ret;
+	uint16_t family, format, outformat;
+
+	if (argc < 3) {
+		printf("Usage: %s {xml|json} <file>\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 
-	nft_rule_snprintf(reprint, sizeof(reprint), r, NFT_OUTPUT_JSON, 0);
-	printf("Parsed:\n%s\n", reprint);
+	if (strcmp(argv[1], "xml") == 0) {
+		format = NFT_PARSE_XML;
+		outformat = NFT_OUTPUT_XML;
+	} else if (strcmp(argv[1], "json") == 0) {
+		format = NFT_PARSE_JSON;
+		outformat = NFT_OUTPUT_JSON;
+	} else {
+		printf("Unknown format: xml, json\n");
+		exit(EXIT_FAILURE);
+	}
 
-	nft_rule_attr_unset(r, NFT_RULE_ATTR_HANDLE);
-	family = nft_rule_attr_get_u8(r, NFT_RULE_ATTR_FAMILY);
+	r = rule_parse_file(argv[2], format);
+	if (r == NULL)
+		exit(EXIT_FAILURE);
+
+	nft_rule_fprintf(stdout, r, outformat, 0);
+	fprintf(stdout, "\n");
+
+	family = nft_rule_attr_get_u32(r, NFT_RULE_ATTR_FAMILY);
 
 	seq = time(NULL);
 	nlh = nft_rule_nlmsg_build_hdr(buf, NFT_MSG_NEWRULE, family,
@@ -87,7 +113,6 @@ int main(int argc, char *argv[])
 				       seq);
 	nft_rule_nlmsg_build_payload(nlh, r);
 	nft_rule_free(r);
-	nft_parse_err_free(err);
 
 	nl = mnl_socket_open(NETLINK_NETFILTER);
 	if (nl == NULL) {
