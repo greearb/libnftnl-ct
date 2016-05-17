e4e00c94a2591ef5367d559a4087dde3071e7833
Carlos Falgueras García <carlosfg@riseup.net>
2016-05-17 18:00:14 +0200

    tests: Free nftnl_udata_buf before exit
    Signed-off-by: Carlos Falgueras García <carlosfg@riseup.net>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- tests/nft-rule-test.c
+++ tests/nft-rule-test.c
@@ -88,6 +88,7 @@ int main(int argc, char *argv[])
 	nftnl_rule_set_data(a, NFTNL_RULE_USERDATA,
 			    nftnl_udata_buf_data(udata),
 			    nftnl_udata_buf_len(udata));
+	nftnl_udata_buf_free(udata);
 
 	nlh = nftnl_rule_nlmsg_build_hdr(buf, NFT_MSG_NEWRULE, AF_INET, 0, 1234);
 	nftnl_rule_nlmsg_build_payload(nlh, a);
