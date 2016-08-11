greearb/libnftnl-ct

commit 7769cbd9dfe69cb653757c681c97693f7e900010

expr: limit: add per-byte limiting support
Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

src/expr/limit.c
@@ -26,6 +26,7 @@ struct nftnl_expr_limit {
 	uint64_t		rate;
 	uint64_t		unit;
 	uint32_t		burst;
+	enum nft_limit_type	type;
 };
 
 static int
@@ -43,6 +44,9 @@ nftnl_expr_limit_set(struct nftnl_expr *e, uint16_t type,
 		break;
 	case NFTNL_EXPR_LIMIT_BURST:
 		limit->burst = *((uint32_t *)data);
+		break;
+	case NFTNL_EXPR_LIMIT_TYPE:
+		limit->type = *((uint32_t *)data);
 		break;
 	default:
 		return -1;
@@ -66,6 +70,9 @@ nftnl_expr_limit_get(const struct nftnl_expr *e, uint16_t type,
 	case NFTNL_EXPR_LIMIT_BURST:
 		*data_len = sizeof(uint32_t);
 		return &limit->burst;
+	case NFTNL_EXPR_LIMIT_TYPE:
+		*data_len = sizeof(uint32_t);
+		return &limit->type;
 	}
 	return NULL;
 }
@@ -85,6 +92,7 @@ static int nftnl_expr_limit_cb(const struct nlattr *attr, void *data)
 			abi_breakage();
 		break;
 	case NFTA_LIMIT_BURST:
+	case NFTA_LIMIT_TYPE:
 		if (mnl_attr_validate(attr, MNL_TYPE_U32) < 0)
 			abi_breakage();
 		break;
@@ -105,6 +113,8 @@ nftnl_expr_limit_build(struct nlmsghdr *nlh, struct nftnl_expr *e)
 		mnl_attr_put_u64(nlh, NFTA_LIMIT_UNIT, htobe64(limit->unit));
 	if (e->flags & (1 << NFTNL_EXPR_LIMIT_BURST))
 		mnl_attr_put_u32(nlh, NFTA_LIMIT_BURST, htonl(limit->burst));
+	if (e->flags & (1 << NFTNL_EXPR_LIMIT_TYPE))
+		mnl_attr_put_u32(nlh, NFTA_LIMIT_TYPE, htonl(limit->type));
 }
 
 static int
@@ -128,6 +138,10 @@ nftnl_expr_limit_parse(struct nftnl_expr *e, struct nlattr *attr)
 		limit->burst = ntohl(mnl_attr_get_u32(tb[NFTA_LIMIT_BURST]));
 		e->flags |= (1 << NFTNL_EXPR_LIMIT_BURST);
 	}
+	if (tb[NFTA_LIMIT_TYPE]) {
+		limit->type = ntohl(mnl_attr_get_u32(tb[NFTA_LIMIT_TYPE]));
+		e->flags |= (1 << NFTNL_EXPR_LIMIT_TYPE);
+	}
 
 	return 0;
 }
@@ -146,6 +160,8 @@ static int nftnl_expr_limit_json_parse(struct nftnl_expr *e, json_t *root,
 		nftnl_expr_set_u64(e, NFTNL_EXPR_LIMIT_UNIT, uval64);
 	if (nftnl_jansson_parse_val(root, "burst", NFTNL_TYPE_U32, &uval32, err) == 0)
 		nftnl_expr_set_u32(e, NFTNL_EXPR_LIMIT_BURST, uval32);
+	if (nftnl_jansson_parse_val(root, "type", NFTNL_TYPE_U32, &uval32, err) == 0)
+		nftnl_expr_set_u32(e, NFTNL_EXPR_LIMIT_TYPE, uval32);
 
 	return 0;
 #else
@@ -160,7 +176,7 @@ static int nftnl_expr_limit_xml_parse(struct nftnl_expr *e,
 {
 #ifdef XML_PARSING
 	uint64_t rate, unit;
-	uint32_t burst;
+	uint32_t burst, type;
 
 	if (nftnl_mxml_num_parse(tree, "rate", MXML_DESCEND_FIRST, BASE_DEC,
 			       &rate, NFTNL_TYPE_U64, NFTNL_XML_MAND, err) == 0)
@@ -172,6 +188,9 @@ static int nftnl_expr_limit_xml_parse(struct nftnl_expr *e,
 	if (nftnl_mxml_num_parse(tree, "burst", MXML_DESCEND_FIRST, BASE_DEC,
 			       &burst, NFTNL_TYPE_U32, NFTNL_XML_MAND, err) == 0)
 		nftnl_expr_set_u32(e, NFTNL_EXPR_LIMIT_BURST, burst);
+	if (nftnl_mxml_num_parse(tree, "type", MXML_DESCEND_FIRST, BASE_DEC,
+			       &burst, NFT_TYPE_U32, NFTNL_XML_MAND, err) == 0)
+		nftnl_expr_set_u32(e, NFTNL_EXPR_LIMIT_TYPE, type);
 
 	return 0;
 #else
@@ -204,8 +223,22 @@ static int nftnl_expr_limit_export(char *buf, size_t size,
 		nftnl_buf_u64(&b, type, limit->unit, UNIT);
 	if (e->flags & (1 << NFTNL_EXPR_LIMIT_BURST))
 		nftnl_buf_u32(&b, type, limit->burst, BURST);
+	if (e->flags & (1 << NFTNL_EXPR_LIMIT_TYPE))
+		nftnl_buf_u32(&b, type, limit->type, TYPE);
 
 	return nftnl_buf_done(&b);
+}
+
+static const char *limit_to_type(enum nft_limit_type type)
+{
+	switch (type) {
+	default:
+	case NFT_LIMIT_PKTS:
+		return "packets";
+	case NFT_LIMIT_PKT_BYTES:
+		return "bytes";
+	}
+	return "unknown";
 }
 
 static int nftnl_expr_limit_snprintf_default(char *buf, size_t len,
@@ -213,8 +246,9 @@ static int nftnl_expr_limit_snprintf_default(char *buf, size_t len,
 {
 	struct nftnl_expr_limit *limit = nftnl_expr_data(e);
 
-	return snprintf(buf, len, "rate %"PRIu64"/%s burst %u ",
-			limit->rate, get_unit(limit->unit), limit->burst);
+	return snprintf(buf, len, "rate %"PRIu64"/%s burst %u type %s ",
+			limit->rate, get_unit(limit->unit), limit->burst,
+			limit_to_type(limit->type));
 }
 
 static int

include/libnftnl/expr.h
@@ -151,6 +151,7 @@ enum {
 	NFTNL_EXPR_LIMIT_RATE	= NFTNL_EXPR_BASE,
 	NFTNL_EXPR_LIMIT_UNIT,
 	NFTNL_EXPR_LIMIT_BURST,
+	NFTNL_EXPR_LIMIT_TYPE,
 };
 
 enum {

include/linux/netfilter/nf_tables.h
@@ -756,18 +756,25 @@ enum nft_ct_attributes {
 };
 #define NFTA_CT_MAX		(__NFTA_CT_MAX - 1)
 
+enum nft_limit_type {
+	NFT_LIMIT_PKTS,
+	NFT_LIMIT_PKT_BYTES
+};
+
 /**
  * enum nft_limit_attributes - nf_tables limit expression netlink attributes
  *
  * @NFTA_LIMIT_RATE: refill rate (NLA_U64)
  * @NFTA_LIMIT_UNIT: refill unit (NLA_U64)
  * @NFTA_LIMIT_BURST: burst (NLA_U32)
+ * @NFTA_LIMIT_TYPE: type of limit (NLA_U32: enum nft_limit_type)
  */
 enum nft_limit_attributes {
 	NFTA_LIMIT_UNSPEC,
 	NFTA_LIMIT_RATE,
 	NFTA_LIMIT_UNIT,
 	NFTA_LIMIT_BURST,
+	NFTA_LIMIT_TYPE,
 	__NFTA_LIMIT_MAX
 };
 #define NFTA_LIMIT_MAX		(__NFTA_LIMIT_MAX - 1)

tests/nft-expr_limit-test.c
@@ -40,6 +40,9 @@ static void cmp_nftnl_expr(struct nftnl_expr *rule_a,
 	if (nftnl_expr_get_u64(rule_a, NFTNL_EXPR_LIMIT_BURST) !=
 	    nftnl_expr_get_u64(rule_b, NFTNL_EXPR_LIMIT_BURST))
 		print_err("Expr CTR_PACKET mismatches");
+	if (nftnl_expr_get_u64(rule_a, NFTNL_EXPR_LIMIT_TYPE) !=
+	    nftnl_expr_get_u64(rule_b, NFTNL_EXPR_LIMIT_TYPE))
+		print_err("Expr TYPE mismatches");
 }
 
 int main(int argc, char *argv[])
@@ -62,6 +65,7 @@ int main(int argc, char *argv[])
 	nftnl_expr_set_u64(ex, NFTNL_EXPR_LIMIT_RATE, 0x123456789abcdef0);
 	nftnl_expr_set_u64(ex, NFTNL_EXPR_LIMIT_UNIT, 0x123456789abcdef0);
 	nftnl_expr_set_u32(ex, NFTNL_EXPR_LIMIT_BURST, 0x89123456);
+	nftnl_expr_set_u32(ex, NFTNL_EXPR_LIMIT_TYPE, 0xdef01234);
 
 	nftnl_rule_add_expr(a, ex);
 

commit e381cd99e9eb0e9519a976c8288f6b9e051ada3a

expr/limit: Drop unreachable code in limit_to_type()
The function returns from inside the switch() in any case, so the final
return statement is never reached.

Fixes: 7769cbd9dfe69 ("expr: limit: add per-byte limiting support")
Signed-off-by: Phil Sutter <phil@nwl.cc>
Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

src/expr/limit.c
@@ -259,7 +259,6 @@ static const char *limit_to_type(enum nft_limit_type type)
 	case NFT_LIMIT_PKT_BYTES:
 		return "bytes";
 	}
-	return "unknown";
 }
 
 static int nftnl_expr_limit_snprintf_default(char *buf, size_t len,