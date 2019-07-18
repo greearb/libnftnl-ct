2e8cbec5c37002d92ca48b52da22a18219bed831
Phil Sutter <phil@nwl.cc>
2019-07-18 05:38:29 +0200

    expr: meta: Make NFT_META_{I,O}IFKIND known
    This only affects debug output, the key was properly handled in
    productive code paths already.
    
    Signed-off-by: Phil Sutter <phil@nwl.cc>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- src/expr/meta.c
+++ src/expr/meta.c
@@ -22,7 +22,7 @@
 #include <libnftnl/rule.h>
 
 #ifndef NFT_META_MAX
-#define NFT_META_MAX (NFT_META_SECPATH + 1)
+#define NFT_META_MAX (NFT_META_OIFKIND + 1)
 #endif
 
 struct nftnl_expr_meta {
@@ -159,6 +159,8 @@ static const char *meta_key2str_array[NFT_META_MAX] = {
 	[NFT_META_CGROUP]	= "cgroup",
 	[NFT_META_PRANDOM]	= "prandom",
 	[NFT_META_SECPATH]	= "secpath",
+	[NFT_META_IIFKIND]	= "iifkind",
+	[NFT_META_OIFKIND]	= "oifkind",
 };
 
 static const char *meta_key2str(uint8_t key)
