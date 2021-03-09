greearb/libnftnl-ct

commit 80077787f8f21da1efd8dc27a4c5767ab47a1df6

src: remove json support
We have better json support in libnftables these days.

Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

src/expr/xfrm.c
@@ -199,66 +199,6 @@ static int str2xfmrdir(const char *s)
 }
 #endif
 
-static int nftnl_expr_xfrm_json_parse(struct nftnl_expr *e, json_t *root,
-				      struct nftnl_parse_err *err)
-{
-#ifdef JSON_PARSING
-	const char *key_str, *dir_str;
-	uint32_t reg, key, spnum;
-	uint8_t dir;
-
-	if (nftnl_jansson_parse_reg(root, "dreg", NFTNL_TYPE_U32, &reg, err) == 0)
-		nftnl_expr_set_u32(e, NFTNL_EXPR_XFRM_DREG, reg);
-
-	key_str = nftnl_jansson_parse_str(root, "key", err);
-	if (key_str != NULL) {
-		key = str2xfrmkey(key_str);
-		if (key < 0)
-			return -1;
-
-		nftnl_expr_set_u32(e, NFTNL_EXPR_XFRM_KEY, key);
-	}
-
-	dir_str = nftnl_jansson_parse_str(root, "dir", err);
-	if (dir_str != NULL) {
-		dir = str2xfmrdir(dir_str);
-		if (dir == -1) {
-			err->node_name = "dir";
-			err->error = NFTNL_PARSE_EBADTYPE;
-			goto err;
-		}
-		nftnl_expr_set_u8(e, NFTNL_EXPR_XFRM_DIR, dir);
-	}
-
-	return 0;
-err:
-	errno = EINVAL;
-	return -1;
-#else
-	errno = EOPNOTSUPP;
-	return -1;
-#endif
-}
-
-static int
-nftnl_expr_xfrm_export(char *buf, size_t size, const struct nftnl_expr *e,
-		     int type)
-{
-	struct nftnl_expr_xfrm *x = nftnl_expr_data(e);
-	NFTNL_BUF_INIT(b, buf, size);
-
-	if (e->flags & (1 << NFTNL_EXPR_XFRM_DREG))
-		nftnl_buf_u32(&b, type, x->dreg, DREG);
-	if (e->flags & (1 << NFTNL_EXPR_XFRM_KEY))
-		nftnl_buf_str(&b, type, xfrmkey2str(x->key), KEY);
-	if (e->flags & (1 << NFTNL_EXPR_XFRM_DIR))
-		nftnl_buf_str(&b, type, xfrmdir2str(x->dir), DIR);
-	if (e->flags & (1 << NFTNL_EXPR_XFRM_SPNUM))
-		nftnl_buf_u32(&b, type, x->spnum, NUM);
-
-	return nftnl_buf_done(&b);
-}
-
 static int
 nftnl_expr_xfrm_snprintf_default(char *buf, size_t size,
 			       const struct nftnl_expr *e)
@@ -285,7 +225,6 @@ nftnl_expr_xfrm_snprintf(char *buf, size_t len, uint32_t type,
 		return nftnl_expr_xfrm_snprintf_default(buf, len, e);
 	case NFTNL_OUTPUT_XML:
 	case NFTNL_OUTPUT_JSON:
-		return nftnl_expr_xfrm_export(buf, len, e, type);
 	default:
 		break;
 	}
@@ -321,5 +260,4 @@ struct expr_ops expr_ops_xfrm = {
 	.parse		= nftnl_expr_xfrm_parse,
 	.build		= nftnl_expr_xfrm_build,
 	.snprintf	= nftnl_expr_xfrm_snprintf,
-	.json_parse	= nftnl_expr_xfrm_json_parse,
 };

commit 54256a0401195e16f4c55eda849d28a264b55208

expr/xfrm: Kill dead code
These functions were used by removed JSON parser only.

Fixes: 80077787f8f21 ("src: remove json support")
Signed-off-by: Phil Sutter <phil@nwl.cc>

src/expr/xfrm.c
@@ -171,34 +171,6 @@ static const char *xfrmdir2str(uint8_t dir)
 	return xfrmdir2str_array[dir];
 }
 
-#ifdef JSON_PARSING
-static uint32_t str2xfrmkey(const char *s)
-{
-	int i;
-
-	for (i = 0;
-	     i < sizeof(xfrmkey2str_array) / sizeof(xfrmkey2str_array[0]);
-	     i++) {
-		if (strcmp(xfrmkey2str_array[i], s) == 0)
-			return i;
-	}
-	return -1;
-}
-
-static int str2xfmrdir(const char *s)
-{
-	int i;
-
-	for (i = 0;
-	     i <  sizeof(xfrmdir2str_array) / sizeof(xfrmdir2str_array[0]);
-	     i++) {
-		if (strcmp(xfrmkey2str_array[i], s) == 0)
-			return i;
-	}
-	return -1;
-}
-#endif
-
 static int
 nftnl_expr_xfrm_snprintf_default(char *buf, size_t size,
 			       const struct nftnl_expr *e)