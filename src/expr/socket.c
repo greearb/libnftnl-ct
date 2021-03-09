greearb/libnftnl-ct

commit 038d226f2e6cc132de151cc295ea2e4b8805659a

src: Add support for native socket matching
Signed-off-by: Máté Eckl <ecklm94@gmail.com>
Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

src/expr/socket.c
@@ -1,0 +1,208 @@
+/*
+ * Copyright (c) 2018 Máté Eckl <ecklm94@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <linux/netfilter/nf_tables.h>
+
+#include "internal.h"
+#include <libmnl/libmnl.h>
+#include <libnftnl/expr.h>
+#include <libnftnl/rule.h>
+
+struct nftnl_expr_socket {
+	enum nft_socket_keys	key;
+	enum nft_registers	dreg;
+};
+
+static int
+nftnl_expr_socket_set(struct nftnl_expr *e, uint16_t type,
+		       const void *data, uint32_t data_len)
+{
+	struct nftnl_expr_socket *socket = nftnl_expr_data(e);
+
+	switch (type) {
+	case NFTNL_EXPR_SOCKET_KEY:
+		socket->key = *((uint32_t *)data);
+		break;
+	case NFTNL_EXPR_SOCKET_DREG:
+		socket->dreg = *((uint32_t *)data);
+		break;
+	default:
+		return -1;
+	}
+	return 0;
+}
+
+static const void *
+nftnl_expr_socket_get(const struct nftnl_expr *e, uint16_t type,
+		       uint32_t *data_len)
+{
+	struct nftnl_expr_socket *socket = nftnl_expr_data(e);
+
+	switch (type) {
+	case NFTNL_EXPR_SOCKET_KEY:
+		*data_len = sizeof(socket->key);
+		return &socket->key;
+	case NFTNL_EXPR_SOCKET_DREG:
+		*data_len = sizeof(socket->dreg);
+		return &socket->dreg;
+	}
+	return NULL;
+}
+
+static int nftnl_expr_socket_cb(const struct nlattr *attr, void *data)
+{
+	const struct nlattr **tb = data;
+	int type = mnl_attr_get_type(attr);
+
+	if (mnl_attr_type_valid(attr, NFTA_SOCKET_MAX) < 0)
+		return MNL_CB_OK;
+
+	switch (type) {
+	case NFTA_SOCKET_KEY:
+	case NFTA_SOCKET_DREG:
+		if (mnl_attr_validate(attr, MNL_TYPE_U32) < 0)
+			abi_breakage();
+		break;
+	}
+
+	tb[type] = attr;
+	return MNL_CB_OK;
+}
+
+static void
+nftnl_expr_socket_build(struct nlmsghdr *nlh, const struct nftnl_expr *e)
+{
+	struct nftnl_expr_socket *socket = nftnl_expr_data(e);
+
+	if (e->flags & (1 << NFTNL_EXPR_SOCKET_KEY))
+		mnl_attr_put_u32(nlh, NFTA_SOCKET_KEY, htonl(socket->key));
+	if (e->flags & (1 << NFTNL_EXPR_SOCKET_DREG))
+		mnl_attr_put_u32(nlh, NFTA_SOCKET_DREG, htonl(socket->dreg));
+}
+
+static int
+nftnl_expr_socket_parse(struct nftnl_expr *e, struct nlattr *attr)
+{
+	struct nftnl_expr_socket *socket = nftnl_expr_data(e);
+	struct nlattr *tb[NFTA_SOCKET_MAX+1] = {};
+
+	if (mnl_attr_parse_nested(attr, nftnl_expr_socket_cb, tb) < 0)
+		return -1;
+
+	if (tb[NFTA_SOCKET_KEY]) {
+		socket->key = ntohl(mnl_attr_get_u32(tb[NFTA_SOCKET_KEY]));
+		e->flags |= (1 << NFTNL_EXPR_SOCKET_KEY);
+	}
+	if (tb[NFTA_SOCKET_DREG]) {
+		socket->dreg = ntohl(mnl_attr_get_u32(tb[NFTA_SOCKET_DREG]));
+		e->flags |= (1 << NFTNL_EXPR_SOCKET_DREG);
+	}
+
+	return 0;
+}
+
+static const char *socket_key2str_array[NFT_SOCKET_MAX + 1] = {
+	[NFT_SOCKET_TRANSPARENT] = "transparent",
+};
+
+static const char *socket_key2str(uint8_t key)
+{
+	if (key < NFT_SOCKET_MAX + 1)
+		return socket_key2str_array[key];
+
+	return "unknown";
+}
+
+static inline int str2socket_key(const char *str)
+{
+	int i;
+
+	for (i = 0; i < NFT_SOCKET_MAX + 1; i++) {
+		if (strcmp(str, socket_key2str_array[i]) == 0)
+			return i;
+	}
+
+	errno = EINVAL;
+	return -1;
+}
+
+static int
+nftnl_expr_socket_snprintf_default(char *buf, size_t len,
+			       const struct nftnl_expr *e)
+{
+	struct nftnl_expr_socket *socket = nftnl_expr_data(e);
+
+	if (e->flags & (1 << NFTNL_EXPR_SOCKET_DREG)) {
+		return snprintf(buf, len, "load %s => reg %u ",
+				socket_key2str(socket->key), socket->dreg);
+	}
+	return 0;
+}
+
+static int nftnl_expr_socket_export(char *buf, size_t size,
+				  const struct nftnl_expr *e, int type)
+{
+	struct nftnl_expr_socket *socket = nftnl_expr_data(e);
+	NFTNL_BUF_INIT(b, buf, size);
+
+	if (e->flags & (1 << NFTNL_EXPR_SOCKET_DREG))
+		nftnl_buf_u32(&b, type, socket->dreg, DREG);
+	if (e->flags & (1 << NFTNL_EXPR_SOCKET_KEY))
+		nftnl_buf_str(&b, type, socket_key2str(socket->key), KEY);
+
+	return nftnl_buf_done(&b);
+}
+
+static int
+nftnl_expr_socket_snprintf(char *buf, size_t len, uint32_t type,
+		       uint32_t flags, const struct nftnl_expr *e)
+{
+	switch (type) {
+	case NFTNL_OUTPUT_DEFAULT:
+		return nftnl_expr_socket_snprintf_default(buf, len, e);
+	case NFTNL_OUTPUT_XML:
+	case NFTNL_OUTPUT_JSON:
+		return nftnl_expr_socket_export(buf, len, e, type);
+	default:
+		break;
+	}
+	return -1;
+}
+
+static bool nftnl_expr_socket_cmp(const struct nftnl_expr *e1,
+			      const struct nftnl_expr *e2)
+{
+	struct nftnl_expr_socket *r1 = nftnl_expr_data(e1);
+	struct nftnl_expr_socket *r2 = nftnl_expr_data(e2);
+	bool eq = true;
+
+	if (e1->flags & (1 << NFTNL_EXPR_SOCKET_KEY))
+		eq &= (r1->key == r2->key);
+	if (e1->flags & (1 << NFTNL_EXPR_SOCKET_DREG))
+		eq &= (r1->dreg == r2->dreg);
+
+	return eq;
+}
+
+struct expr_ops expr_ops_socket = {
+	.name		= "socket",
+	.alloc_len	= sizeof(struct nftnl_expr_socket),
+	.max_attr	= NFTA_SOCKET_MAX,
+	.cmp		= nftnl_expr_socket_cmp,
+	.set		= nftnl_expr_socket_set,
+	.get		= nftnl_expr_socket_get,
+	.parse		= nftnl_expr_socket_parse,
+	.build		= nftnl_expr_socket_build,
+	.snprintf	= nftnl_expr_socket_snprintf,
+};

include/libnftnl/expr.h
@@ -73,6 +73,11 @@ enum {
 };
 
 enum {
+	NFTNL_EXPR_SOCKET_KEY	= NFTNL_EXPR_BASE,
+	NFTNL_EXPR_SOCKET_DREG,
+};
+
+enum {
 	NFTNL_EXPR_CMP_SREG	= NFTNL_EXPR_BASE,
 	NFTNL_EXPR_CMP_OP,
 	NFTNL_EXPR_CMP_DATA,

include/linux/netfilter/nf_tables.h
@@ -905,6 +905,31 @@ enum nft_rt_attributes {
 #define NFTA_RT_MAX		(__NFTA_RT_MAX - 1)
 
 /**
+ * enum nft_socket_attributes - nf_tables socket expression netlink attributes
+ *
+ * @NFTA_SOCKET_KEY: socket key to match
+ * @NFTA_SOCKET_DREG: destination register
+ */
+enum nft_socket_attributes {
+	NFTA_SOCKET_UNSPEC,
+	NFTA_SOCKET_KEY,
+	NFTA_SOCKET_DREG,
+	__NFTA_SOCKET_MAX
+};
+#define NFTA_SOCKET_MAX		(__NFTA_SOCKET_MAX - 1)
+
+/*
+ * enum nft_socket_keys - nf_tables socket expression keys
+ *
+ * @NFT_SOCKET_TRANSPARENT: Value of the IP(V6)_TRANSPARENT socket option_
+ */
+enum nft_socket_keys {
+	NFT_SOCKET_TRANSPARENT,
+	__NFT_SOCKET_MAX
+};
+#define NFT_SOCKET_MAX	(__NFT_SOCKET_MAX - 1)
+
+/**
  * enum nft_ct_keys - nf_tables ct expression keys
  *
  * @NFT_CT_STATE: conntrack state (bitmask of enum ip_conntrack_info)

src/Makefile.am
@@ -54,6 +54,7 @@ libnftnl_la_SOURCES = utils.c		\
 		      expr/masq.c	\
 		      expr/redir.c	\
 		      expr/hash.c	\
+		      expr/socket.c	\
 		      obj/counter.c	\
 		      obj/ct_helper.c	\
 		      obj/quota.c	\

src/expr_ops.c
@@ -34,6 +34,7 @@ extern struct expr_ops expr_ops_dynset;
 extern struct expr_ops expr_ops_hash;
 extern struct expr_ops expr_ops_fib;
 extern struct expr_ops expr_ops_flow;
+extern struct expr_ops expr_ops_socket;
 
 static struct expr_ops expr_ops_notrack = {
 	.name	= "notrack",
@@ -71,6 +72,7 @@ static struct expr_ops *expr_ops[] = {
 	&expr_ops_fib,
 	&expr_ops_objref,
 	&expr_ops_flow,
+	&expr_ops_socket,
 	NULL,
 };
 

commit 100708a7e720e17a7c46a6d414ec3fb663ee01aa

expr/socket: Kill dead code
Function str2socket_key() was never used.

Fixes: 038d226f2e6cc ("src: Add support for native socket matching")
Signed-off-by: Phil Sutter <phil@nwl.cc>

src/expr/socket.c
@@ -126,19 +126,6 @@ static const char *socket_key2str(uint8_t key)
 	return "unknown";
 }
 
-static inline int str2socket_key(const char *str)
-{
-	int i;
-
-	for (i = 0; i < NFT_SOCKET_MAX + 1; i++) {
-		if (strcmp(str, socket_key2str_array[i]) == 0)
-			return i;
-	}
-
-	errno = EINVAL;
-	return -1;
-}
-
 static int
 nftnl_expr_socket_snprintf_default(char *buf, size_t len,
 			       const struct nftnl_expr *e)