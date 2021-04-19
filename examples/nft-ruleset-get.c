c335442eefcca160fb00f033b236deea61bd7b10
Pablo Neira Ayuso <pablo@netfilter.org>
2021-04-20 01:57:20 +0200

    src: incorrect header refers to GPLv2 only
    libnftnl is GPLv2+, fix incorrect headers.
    
    Acked-by: Arturo Borrero Gonzalez <arturo@netfilter.org>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- examples/nft-ruleset-get.c
+++ examples/nft-ruleset-get.c
@@ -5,9 +5,9 @@
  *
  * Copyright (c) 2013 Pablo Neira Ayuso <pablo@netfilter.org>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
  */
 
--- src/batch.c
+++ src/batch.c
@@ -2,8 +2,9 @@
  * Copyright (c) 2013-2015 Pablo Neira Ayuso <pablo@netfilter.org>
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  */
 
 #include "internal.h"
