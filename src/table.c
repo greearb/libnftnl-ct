greearb/libnftnl-ct

commit 985955fe41f53019d0def894cf9b697228e4b774

table: add table owner support
Add support for NFTA_TABLE_OWNER.

Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

src/table.c
@@ -34,6 +34,7 @@ struct nftnl_table {
 	uint64_t 	handle;
 	uint32_t	use;
 	uint32_t	flags;
+	uint32_t	owner;
 	struct {
 		void		*data;
 		uint32_t	len;
@@ -76,8 +77,8 @@ void nftnl_table_unset(struct nftnl_table *t, uint16_t attr)
 	case NFTNL_TABLE_FLAGS:
 	case NFTNL_TABLE_HANDLE:
 	case NFTNL_TABLE_FAMILY:
-		break;
 	case NFTNL_TABLE_USE:
+	case NFTNL_TABLE_OWNER:
 		break;
 	}
 	t->flags &= ~(1 << attr);
@@ -126,6 +127,9 @@ int nftnl_table_set_data(struct nftnl_table *t, uint16_t attr,
 			return -1;
 		memcpy(t->user.data, data, data_len);
 		t->user.len = data_len;
+		break;
+	case NFTNL_TABLE_OWNER:
+		memcpy(&t->owner, data, sizeof(t->owner));
 		break;
 	}
 	t->flags |= (1 << attr);
@@ -188,6 +192,9 @@ const void *nftnl_table_get_data(const struct nftnl_table *t, uint16_t attr,
 	case NFTNL_TABLE_USERDATA:
 		*data_len = t->user.len;
 		return t->user.data;
+	case NFTNL_TABLE_OWNER:
+		*data_len = sizeof(uint32_t);
+		return &t->owner;
 	}
 	return NULL;
 }
@@ -258,6 +265,7 @@ static int nftnl_table_parse_attr_cb(const struct nlattr *attr, void *data)
 		break;
 	case NFTA_TABLE_FLAGS:
 	case NFTA_TABLE_USE:
+	case NFTA_TABLE_OWNER:
 		if (mnl_attr_validate(attr, MNL_TYPE_U32) < 0)
 			abi_breakage();
 		break;
@@ -307,6 +315,10 @@ int nftnl_table_nlmsg_parse(const struct nlmsghdr *nlh, struct nftnl_table *t)
 			mnl_attr_get_payload_len(tb[NFTA_TABLE_USERDATA]));
 		if (ret < 0)
 			return ret;
+	}
+	if (tb[NFTA_TABLE_OWNER]) {
+		t->owner = ntohl(mnl_attr_get_u32(tb[NFTA_TABLE_OWNER]));
+		t->flags |= (1 << NFTNL_TABLE_OWNER);
 	}
 
 	t->family = nfg->nfgen_family;

include/libnftnl/table.h
@@ -24,6 +24,7 @@ enum nftnl_table_attr {
 	NFTNL_TABLE_USE,
 	NFTNL_TABLE_HANDLE,
 	NFTNL_TABLE_USERDATA,
+	NFTNL_TABLE_OWNER,
 	__NFTNL_TABLE_MAX
 };
 #define NFTNL_TABLE_MAX (__NFTNL_TABLE_MAX - 1)

include/linux/netfilter/nf_tables.h
@@ -181,6 +181,7 @@ enum nft_table_attributes {
 	NFTA_TABLE_HANDLE,
 	NFTA_TABLE_PAD,
 	NFTA_TABLE_USERDATA,
+	NFTA_TABLE_OWNER,
 	__NFTA_TABLE_MAX
 };
 #define NFTA_TABLE_MAX		(__NFTA_TABLE_MAX - 1)

commit 08c9cab3352402c1a7d7952d1a2ce0a051f48b14

table: Validate NFTNL_TABLE_OWNER, too
Fixes: 985955fe41f53 ("table: add table owner support")
Signed-off-by: Phil Sutter <phil@nwl.cc>

src/table.c
@@ -89,6 +89,7 @@ static uint32_t nftnl_table_validate[NFTNL_TABLE_MAX + 1] = {
 	[NFTNL_TABLE_FAMILY]	= sizeof(uint32_t),
 	[NFTNL_TABLE_HANDLE]	= sizeof(uint64_t),
 	[NFTNL_TABLE_USE]	= sizeof(uint32_t),
+	[NFTNL_TABLE_OWNER]	= sizeof(uint32_t),
 };
 
 EXPORT_SYMBOL(nftnl_table_set_data);